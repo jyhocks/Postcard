1b6f8ac8804ae0f3791298d8410b7180d566e0d4
Jonathan Hocks <jonathan@Jonathans-Mac-mini.local>
2015-07-11 12:58:58 +0300

    First rev of project

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Jonathan Hocks on 6/24/15.
+//  Copyright (c) 2015 Jonathan Hocks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextFiield: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        sender.backgroundColor = UIColor.redColor()
+        
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.orangeColor()
+        messageLabel.hidden = false
+        
+        enterMessageTextField.text = ""
+        
+        
+                              // Make the keyboard "go away"
+        enterMessageTextField.resignFirstResponder()
+        
+        // here i could just use "sender" instead of "mailButton" if i wanted to.
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
+    
+}
+
